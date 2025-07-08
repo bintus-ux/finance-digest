7a244a548a20a483e3e397d7cde31b1d4e72184f
Ezerike Caleb <cezerike@gmail.com>
2025-07-08 13:33:10 +0100

    'fixes'

--- src/components/News/NewsList.tsx
+++ src/components/News/NewsList.tsx
@@ -3,12 +3,29 @@
 import React from "react";
 import { NewsItem } from "@/types/news";
 import NewsCard from "./NewsCard";
+import NewsSkeleton from "./NewsSkeleton";
 
 interface NewsListProps {
   news: NewsItem[];
+  loading?: boolean;
+  error?: string | null;
 }
 
-const NewsList: React.FC<NewsListProps> = ({ news }) => {
+const NewsList: React.FC<NewsListProps> = ({ news, loading, error }) => {
+  if (error) {
+    return <div className="p-4 text-center text-red-500">{error}</div>;
+  }
+
+  if (loading) {
+    return (
+      <div className="space-y-4">
+        {[...Array(5)].map((_, i) => (
+          <NewsSkeleton key={i} />
+        ))}
+      </div>
+    );
+  }
+
   if (!news.length) {
     return (
       <div className="p-4 text-center text-gray-500">
